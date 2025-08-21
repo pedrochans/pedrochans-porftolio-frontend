ffe24cf59cb16eb51579ecfe0ab52042ee395745
pedrocfa <pedrocfa@ext.inditex.com>
2025-08-21 13:20:18 +0200

    text compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
-  }
+  },
+  integrations: [
+    compress({
+      css: true,
+      html: {
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeEmptyAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: false
+      },
+      js: true,
+      svg: false, // Ya optimizamos las imágenes manualmente
+      img: false
+    })
+  ],
+  build: {
+    inlineStylesheets: 'auto',
+    assets: '_astro'
+  },
+  compressHTML: true
 });
